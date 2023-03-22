d9cdd2c098438bfdd851c192e4e24d776adddb09
CodeMeYulia <yupipper@gmail.com>
2023-03-22 05:06:32 +0300

    call function

---
+++ index.js
@@ -0,0 +1,77 @@
+// function showMessage()
+// {
+//     alert('Миру - мир!')
+// }
+
+// showMessage()
+
+// function sum (a, b)
+// {
+//     return a + b;
+// }
+// alert(sum(8, 7));
+
+
+
+// function y(x){
+//     return x ** 5;
+// }
+// let result = y(5);
+// alert(result);
+
+
+// function showMessageFromUser(name = 'Anonim', message = 'thinking about'){
+//     alert(name + ' :  ' + message);
+//     alert(`${name}: ${message}`);
+// } 
+
+// alert(showMessageFromUser('Юля', 'алоха!'));
+// showMessageFromUser();
+
+
+// function sayHi(from, text){
+//     console.log(from + ':  ' + text);
+// }
+// sayHi('Аня', 'мурмяу, кагдела?');
+// sayHi('Таня', 'замурчательно!');
+
+
+// function sayHi(from, text) {
+
+//     from = '*' + from + '*'; // немного украсим "from" alert( from + ': ' + text );
+//   }
+  
+//   let from = "Аня";
+  
+//   sayHi(from, "Привет"); // *Аня*: Привет
+  
+//   значение "from" осталось прежним, функция изменила значение локальной переменной
+//   alert( from , "Привет"); // Аня
+
+  
+// let sayHi = function() {
+//   console.log( "Алоха" );
+// };
+// sayHi();
+
+
+// let sum = (a, b) => {
+//     let result = a + b;
+//     return result;
+// }
+
+// console.log(sum(1, 2));  //3
+
+function sayOne() {
+    console.log('One');
+}
+
+let sayTwo = function() {
+    console.log('Two');
+}
+
+let sayThree = () => console.log('Three');
+
+sayOne();
+sayTwo();
+sayThree();
